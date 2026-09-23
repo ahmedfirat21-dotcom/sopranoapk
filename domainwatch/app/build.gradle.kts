e3f20fb6ab57fd638258300ff17aab1bb95094ea
ahmedfirat21-dotcom <ahmedfirat21@gmail.com>
2026-09-24 01:46:01 +0300

    Add DomainWatch isolated build

---
+++ domainwatch/app/build.gradle.kts
@@ -0,0 +1,6 @@
+plugins { id("com.android.application") }
+android {
+ namespace = "com.domainwatch.app"
+ compileSdk = 35
+ defaultConfig { applicationId = "com.domainwatch.app"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "1.0" }
+}
